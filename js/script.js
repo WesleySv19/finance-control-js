95ab22e8e1c7a71e070e28c2917f32e9d2a72d56
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-09-03 01:09:25 -0300

    sending js

--- js/script.js
+++ js/script.js
@@ -5,76 +5,17 @@ const type = document.querySelector('#type')
 const btnNew = document.querySelector('#btnNew')
 
 const incomes = document.querySelector('.incomes')
-const expenses = document.querySelector('expenses')
+const expenses = document.querySelector('.expenses')
 const total = document.querySelector('.total')
 
-let items 
-
-function deleteItem (index) {
-    items.splice(index, 1)
-    setItensBd()
-    loadItensBd()
-}   
-
-function insertItem (item, index) {
-    let tr = document.createElement('tr')
-
-    tr.innerHTML = `
-        <td>${item.desc}</td>
-        <td class="columnType">${
-            item.type === 'Entrada'
-            ? ' <i class"bxs-chevron-up-circle" '
-            : '<i class="bxs-chevron-down-circle" '
-        }</td>
-
-        <td class="columnAction">
-            <button onclick"deleteItem(${index})"><i class="bx bx-trash'></i><button/>"
-        </td>
-    `
-
-    tbody.appendChild(tr)
-}
-
-function loadItens () {
-    items = getItensBd()
-    tbody.innerHTML = ''
-    items.forEach((item, index) => {
-    insertItem(item, index)
-    })
-
-    getTotals()
-}
-
-function getTotals () {
-    const amountIncomes = items
-    .filter((item) => item.type === 'Entrada')
-    .map((transaction) => Number(transaction.amount))
-    
-    const amounExpenses = items
-    .filter((item) => item.type === 'Saída')
-    .map((transaction) => Number(transaction.amount))
-
-    const totalIncomes = amountIncomes 
-    .reduce((acc, cur) => acc + cur, 0)
-    .toFixed(2)
-
-    const totalExpenses = Math.abs(
-        amounExpenses.reduce((acc, cur) => acc + cur, 0)
-    ).toFixed(2)
-
-    const totaItens = (totalIncomes - totalExpenses).toFixed(2)
-
-    incomes.innerHTML = totalIncomes
-    expenses.innerHTML = totalExpenses
-    total.innerHTML = totaItens
-}
+let items = []
 
 btnNew.onclick = () => {
-    if(descItem.value === '' || amount.value === '' || type.value === '') {
+    if (descItem.value === '' || amount.value === '' || type.value === '') {
         return alert('Preencha todos os campos!')
     }
 
-    items.push( {
+    items.push({
         desc: descItem.value,
         amount: Math.abs(amount.value).toFixed(2),
         type: type.value
@@ -86,6 +27,65 @@ btnNew.onclick = () => {
     descItem.value = ''
     amount.value = ''
 }
- 
-const getItensBd = () => JSON.parse(localStorage.getItem('db_getItems')) ?? []
+
+function deleteItem(index) {
+    items.splice(index, 1)
+    setItensBd()
+    loadItens()
+}
+
+function insertItem(item, index) {
+    let tr = document.createElement('tr')
+
+    tr.innerHTML = `
+        <td>${item.desc}</td>
+        <td>R$ ${item.amount}</td>
+        <td class="columnType">
+        ${item.type === 'Entrada'
+            ? '<i class="bx bxs-chevron-up-circle"</i>'
+            : '<i class="bx bxs-chevron-down-circle"</i>'
+        }</td>
+
+        <td class="columnAction">
+            <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i><button/>
+        </td>
+    `
+    tbody.appendChild(tr)
+}
+
+function loadItens() {
+    items = getItensBd()
+    tbody.innerHTML = ''
+    items.forEach((item, index) => {
+        insertItem(item, index)
+    })
+    getTotals()
+}
+
+function getTotals() {
+    const amountIncomes = items
+        .filter((item) => item.type === 'Entrada')
+        .map((transaction) => Number(transaction.amount))
+
+    const amounExpenses = items
+        .filter((item) => item.type === 'Saída')
+        .map((transaction) => Number(transaction.amount))
+
+    const totalIncomes = amountIncomes
+        .reduce((acc, cur) => acc + cur, 0)
+        .toFixed(2)
+
+    const totalExpenses = Math.abs(
+        amounExpenses.reduce((acc, cur) => acc + cur, 0)
+    ).toFixed(2)
+
+    const totaItens = (totalIncomes - totalExpenses).toFixed(2)
+    incomes.innerHTML = totalIncomes
+    expenses.innerHTML = totalExpenses
+    total.innerHTML = totaItens
+}
+
+const getItensBd = () => JSON.parse(localStorage.getItem('db_items')) ?? []
 const setItensBd = () => localStorage.setItem('db_items', JSON.stringify(items))
+
+loadItens()
